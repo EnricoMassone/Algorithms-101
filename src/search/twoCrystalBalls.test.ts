6636d66b027d0880f4de21d609f575e447fd2ca6
Enrico Massone <enrico.massone@deltatre.com>
2022-11-02 14:35:53 +0100

    Implemented the two crystal balls problem

---
+++ src/search/twoCrystalBalls.test.ts
@@ -0,0 +1,13 @@
+import { twoCrystalBalls } from "./twoCrystalBalls";
+
+test("Two crystal balls", () => {
+  const firstBreakIndex = Math.floor(Math.random() * 10000);
+  const breaks = new Array<boolean>(10000).fill(false);
+
+  for (let i = firstBreakIndex; i < 10000; ++i) {
+    breaks[i] = true;
+  }
+
+  expect(twoCrystalBalls(breaks)).toEqual(firstBreakIndex);
+  expect(twoCrystalBalls(new Array<boolean>(821).fill(false))).toEqual(-1);
+});
---
+++ src/search/twoCrystalBalls.ts
@@ -0,0 +1,36 @@
+/**
+ * Implements the two crystal balls problem by using an array of boolean values as the input.
+ * From a certain index on, all the array items are True (or all the array items are False).
+ * The algorithm finds the first array index which contains True.The value -1 is returned if all array items are False.
+ * @param { boolean[] } breaks - The array of boolean values used as the input. From a certain index on all the array items are True or all the array items are False.
+ * @returns { number } The first array index which contains the True value or -1 if all array items equal False.
+ */
+export function twoCrystalBalls(breaks: boolean[]): number {
+  // compute the jump amount by using the square root of the array lenght
+  const jumpAmount = Math.floor(Math.sqrt(breaks.length));
+
+  // use the first ball to jump until a breaking point is found.
+  let breakingPointIndex = jumpAmount;
+
+  for (; breakingPointIndex < breaks.length; breakingPointIndex += jumpAmount) {
+    if (breaks[breakingPointIndex]) {
+      break;
+    }
+  }
+
+  // now we go back of jumpAmount of steps from the known breaking point and use the second ball to linear scan
+  // until a breaking point is found.
+  let currentIndex = breakingPointIndex - jumpAmount;
+
+  for (
+    let counter = 0;
+    counter <= jumpAmount && currentIndex < breaks.length;
+    counter++, currentIndex++
+  ) {
+    if (breaks[currentIndex]) {
+      return currentIndex;
+    }
+  }
+
+  return -1;
+}
