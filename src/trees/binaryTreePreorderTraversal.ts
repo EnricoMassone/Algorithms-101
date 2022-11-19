8e9c125df22d9da87f39e047b0c5ce1041c23e6d
Enrico Massone <enrico.massone@deltatre.com>
2022-11-19 23:04:33 +0100

    added js docs to the binary tree preorder traversal

--- src/trees/binaryTreePreorderTraversal.ts
+++ src/trees/binaryTreePreorderTraversal.ts
@@ -8,14 +8,20 @@ function walk<T>(current: BinaryTreeNode<T> | null, path: T[]): void {
 
   /*
    * This is the recursive case.
-   * Since this is a preorder traversal we need to explore the current node, then explore the left subtree
-   * (recursive call) and finally explore the right subtree (recursive call).
+   * Since this is a preorder traversal we need to process the current node, then traverse the left subtree
+   * (first recursive call) and finally traverse the right subtree (second recursive call).
    */
   path.push(current.value);
   walk(current.left, path);
   walk(current.right, path);
 }
 
+/**
+ * Implements a depth-first preorder tree traversal of a binary tree.
+ * @param { BinaryTreeNode<T> } root - The tree root.
+ * @returns { T[] } The array containing the tree nodes values.
+ * @template T The type of values contained in the tree nodes.
+ */
 function traverse<T>(root: BinaryTreeNode<T>): T[] {
   const path: T[] = [];
   walk(root, path);
